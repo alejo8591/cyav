716332063868b0d8e4f383d93c607195f6d85650
Alejandro Romero <alejandro.romero@agilapps.co>
2014-02-10 14:13:54 -0500

    update unit-2 with lab4

---
+++ unidad-2/lab4/js/lab4.js
@@ -0,0 +1,12 @@
+// Combining Two Arrays
+$(document).ready(function(){
+	var membersOne = [34, 55, 56, 76, 89];
+	var membersTwo = [12234, 12233, 445, 454, 322, 122];
+
+	$('.firstArray').html(membersOne.join('<br/>'));
+	$('.secondArray').html(membersTwo.join('<br/>'));
+
+	members = membersOne.concat(membersTwo);
+
+	$('.combineArray').html(members.join('<br/>'));
+});
