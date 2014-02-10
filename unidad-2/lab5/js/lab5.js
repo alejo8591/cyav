9e291c6dc318d1db35e209d98283626f5ca66b30
Alejandro Romero <alejandro.romero@agilapps.co>
2014-02-10 14:23:54 -0500

    update unit-2 with lab5

---
+++ unidad-2/lab5/js/lab5.js
@@ -0,0 +1,17 @@
+//Convert a numeric array into a string and substring search
+$(document).ready(function(){
+	var members = [14, 15, 56 , 8888, 544, 31231243, 321312313];
+
+	$('.originalArray').html(members.join('<br/>'));
+
+	var strs = members.join(" ,");
+
+	$('.stringArray').text(strs);
+
+	var subStr = strs.substr(0, 3);
+
+	console.log(typeof(subStr));
+
+	$('.partArray').text(subStr);
+
+});
