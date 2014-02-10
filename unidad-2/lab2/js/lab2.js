0d875d8e5fb4f9b7ec1f317aacc05309fbd535aa
Alejandro Romero <alejandro.romero@agilapps.co>
2014-02-10 13:42:31 -0500

    Udpate unit-2 with lab2

---
+++ unidad-2/lab2/js/lab2.js
@@ -0,0 +1,10 @@
+//Viewing names in arrays arguments for presentation to recommend'' sencila and double quotes "" to texts 
+//arguments for presentation to recommend'' sencila and double quotes "" to texts
+$(document).ready(function(){
+
+	var members = ["Daniel", "Edwin", "Giovanny", "Alejandro", "Manuel"];
+	members = $.map(members, function(n,i){
+		return(i + 1 + "." + n);
+	});
+	$('p').html(members.join("<br/>"))
+});
