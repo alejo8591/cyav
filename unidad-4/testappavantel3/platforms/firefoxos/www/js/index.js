a5b0a1fdda9873875b4eea23613796a2e039d651
Alejandro Romero <alejo8591@gmail.com>
2014-02-25 12:45:55 -0500

    push new example app

---
+++ unidad-4/testappavantel3/platforms/firefoxos/www/js/index.js
@@ -0,0 +1,66 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicity call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+        checkConnection();
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    }
+};
+
+function checkConnection(){
+        // utilizando el API de PhoneGap o Cordova
+        var networkState = navigator.connection.type;
+        var states = {}
+        // Cargando objeto con las posibles conexiones
+        states[Connection.UNKNOWN]  = 'Conexión no reconocida';
+        states[Connection.WIFI] = 'Conexión por WIFI';
+        states[Connection.CELL_2G] = 'Conexión por 2G';
+        states[Connection.CELL_3G] = 'Conexión por 3G';
+        states[Connection.CELL_4G] = 'Conexión por 4G';
+        states[Connection.CELL] = 'Conexión generia a red Celular';
+        states[Connection.NONE] = 'No esta conectado a ninguna Red';
+        // Conociendo el tipo de conexión de mi dispositivo
+        alert(states[networkState]);
+}
